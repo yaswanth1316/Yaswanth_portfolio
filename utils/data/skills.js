5044623531729d0bf90ab71a72702da724480cb4
yaswanth1316 <73282622+yaswanth1316@users.noreply.github.com>
2025-01-21 13:44:25 -0500

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -16,7 +16,8 @@ export const skillsData = [
   'Wordpress',
   'Bootstrap',
   'Docker',
-  'Figma'
+  'Figma',
+  'aws'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
