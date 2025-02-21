148cbad9cf22b19fa2c2990cea19cad909fc5ba0
yaswanth1316 <73282622+yaswanth1316@users.noreply.github.com>
2025-02-21 18:15:13 -0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profile.png',
   designation: "Software Developer",
   description: "I am Yaswanth Chakiri, a passionate software developer with 3 years of experience and Master's student in Computer Science at Virginia Tech with a GPA of 4.0. With professional experience in building scalable web applications and APIs using Python, React, Flask, Node.js, and MySQL, I specialize in creating user-friendly interfaces and robust backend systems. My expertise extends to cloud platforms like AWS (S3, EC2, RDS) and CI/CD tools such as Git, Docker, and Jenkins. Notable achievements include developing multi-modal imaging software that improved data processing efficiency by 25% and leading projects like a machine-learning health assistant with 93.2% prediction accuracy. I am also certified in AWS Cloud Solutions and Cisco Cyber Ops, showcasing my dedication to cloud computing and cybersecurity. Let’s connect to explore how I can contribute to impactful tech solutions.",
-  email: 'yaswanth22@vt.edu',
+  email: 'yaswanth836@gmail.com',
   phone: '+1 5405583871',
   address: 'Blacksburg, Virginia, United States - 24060 ',
   github: 'https://github.com/yaswanth1316',
