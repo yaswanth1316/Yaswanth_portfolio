8727beb9387ca33af96eca6a45c96e43617a1c55
yaswanth1316 <73282622+yaswanth1316@users.noreply.github.com>
2025-04-08 17:03:39 -0400

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,16 +1,16 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer',
-    company: "UJR Technologies Pvt Ltd",
-    duration: "(Jul 2021 - Dec 2022)",
-    description: "[Python, React, Flask, Node.js, Amazon S3, RESTful API, MySQL, Git]"
+    title: 'Python Full-Stack Developer',
+    company: "NYU Health",
+    duration: "(May 2024 - Present)",
+    description: "[Python, React, Django, React.js, Amazon S3, PostgreSQL, Git]"
   },
   {
     id: 2,
-    title: 'Web Developer',
-    company: "Virginia Tech",
-    duration: "(May 2023 - May 2024)",
+    title: 'Full-Stack Developer',
+    company: "Dentsu",
+    duration: "(Jul 2021 - Dec 2022)",
     description: "[ Python, React, Flask, MySQL, Git]"
   },
   {
